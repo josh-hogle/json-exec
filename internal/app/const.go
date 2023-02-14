3b1fe19b6f75e5a3b9653e4b6d34c2f7619bd0c0
Josh Hogle <josh@joshhogle.com>
2023-02-13 19:37:20 -0500

    Initial commit
    Signed-off-by: Josh Hogle <josh@joshhogle.com>

---
+++ internal/app/const.go
@@ -0,0 +1,9 @@
+package app
+
+const (
+	// Copyright is the copyright information for the program.
+	Copyright = "Copyright (c) 2022 Josh Hogle. All rights reserved."
+
+	// Title is the title of the program.
+	Title = "json-exec"
+)
